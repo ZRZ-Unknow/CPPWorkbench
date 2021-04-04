a1d744fa3461caa50ac39f24f770ea4a8b9f4c58
zrz <zhourz@smail.nju.edu.cn>
2021-04-05 00:02:19 +0800

    finish HW4

--- HW/HW4/date.cpp
+++ HW/HW4/date.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
 
@@ -32,6 +33,84 @@ class Datetime{
 private:
     Date _date;
     Time _time;
+    bool is_leap_year(int year) const{
+        if(year%4 != 0)
+            return false;
+        if(year%100 != 0)
+            return true;
+        if(year%400 != 0)
+            return false;
+        return true;
+    }
+    bool is_31(int month) const{
+        return month==1 || month==3 || month==7 || month==8 || month==10 || month==12;
+    }
+    bool is_30(int month) const{
+        return month==4 || month==6 || month==9 || month==11;
+    }
+    bool is_28(int year, int month) const{
+        return month==2 && !is_leap_year(year);
+    }
+    bool is_29(int year, int month) const{
+        return month==2 && is_leap_year(year);
+    }
+    //increment 1
+    void increment(int &year, int &month, int &day, int &hour, int &minute, int &second) const{
+        if(second<59) second++;
+        else{
+            second = 0;
+            if(minute<59) minute++;
+            else{
+                minute = 0;
+                if(hour<23) hour++;
+                else{
+                    hour = 0;
+                    if((is_31(month) && day<31) || 
+                       (is_30(month) && day<30) ||
+                       (is_29(year, month) && day<29) ||
+                       (is_28(year, month) && day<28)){
+                           day++;
+                    }else{
+                        day = 1;
+                        if(month<12) month++;
+                        else{
+                            month = 1;
+                            year++;
+                        }
+                    }
+                } 
+            }
+        }
+    }
+    //decrement 1
+    void decrement(int &year, int &month, int &day, int &hour, int &minute, int &second) const{
+        if(second>0) second--;
+        else{
+            second = 59;
+            if(minute>0) minute--;
+            else{
+                minute = 59;
+                if(hour>0) hour--;
+                else{
+                    hour = 23;
+                    if(day>1) day--;
+                    else{
+                        if(month==1) day = 31;
+                        else if(is_31(month-1)) day = 31;
+                        else if(is_30(month-1)) day = 30;
+                        else if(is_29(year, month-1)) day = 29;
+                        else if(is_28(year, month-1)) day = 28;
+                        else assert(0); 
+                        if(month>1) month--;
+                        else{
+                            month = 12;
+                            year--;
+                        }
+                    }
+                } 
+            }
+        }
+    }
 public:
     Datetime(const Date &date, const Time &time){
         _date = date;
@@ -57,53 +136,45 @@ public:
                this->_time._minute <= datetime._time._minute &&
                this->_time._second <= datetime._time._second;
     }
-    void add(int &year, int &month, int &day, int &hour, int &minute, int &second, long seconds) const{
-        second += seconds;
-    }
-    void sub(int &year, int &month, int &day, int &hour, int &minute, int &second, long seconds) const{
-        second -= seconds;
-    }
     Datetime operator+(long seconds) const{
         Date date = _date;
         Time time = _time;
         Datetime datetime(date, time);
-        add(datetime._date._year, datetime._date._month, datetime._date._day,
-            datetime._time._hour, datetime._time._minute, datetime._time._second, seconds);
+        for(int i=0;i<seconds;i++)
+            increment(datetime._date._year, datetime._date._month, datetime._date._day,
+                      datetime._time._hour, datetime._time._minute, datetime._time._second);
         return datetime;
     }
     Datetime operator-(long seconds) const{
         Date date = _date;
         Time time = _time;
         Datetime datetime(date, time);
-        sub(datetime._date._year, datetime._date._month, datetime._date._day,
-            datetime._time._hour, datetime._time._minute, datetime._time._second, seconds);
+        for(int i=0;i<seconds;i++)
+            decrement(datetime._date._year, datetime._date._month, datetime._date._day,
+                      datetime._time._hour, datetime._time._minute, datetime._time._second);
         return datetime;
     }
     void operator++(int){
-        add(_date._year, _date._month, _date._day, _time._hour, _time._minute, _time._second, 1);
+        increment(_date._year, _date._month, _date._day, _time._hour, _time._minute, _time._second);
     }
     void operator--(int){
-        sub(_date._year, _date._month, _date._day, _time._hour, _time._minute, _time._second, 1);
+        decrement(_date._year, _date._month, _date._day, _time._hour, _time._minute, _time._second);
     }
-
 };
 
 
 int main(){
-    Date d1(2020, 1, 20);
-    Time t1(20, 1, 40);
-    Date d2(2019, 1, 20);
-    Time t2(20, 1, 29);
+    Date d1(2021, 1, 1);
+    Time t1(0, 0, 40);
     Datetime dt1(d1, t1);
-    Datetime dt2(d1, t2);
-    cout<<(dt2<dt1)<<endl;
-    Datetime dt3 = dt1 + 17;
-    Datetime dt4 = dt3 - 7;
-    dt1++;
-    dt2--;
+    long s = 61;
+    long ss = 60;
+    Datetime dt2 = dt1 + s;
     dt1.print();
+    printf("add %d seconds\n", s); 
     dt2.print();
+    printf("sub %d seconds\n", ss);
+    Datetime dt3 = dt1 - ss;
     dt3.print();
-    dt4.print();
     return 0;
 }
